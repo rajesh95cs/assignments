10bbbaa852aa2a961494a9ad1742b3a85a386723
rajesh95cs <rajeshrnair95@example.com>
2016-07-18 01:25:36 +0530

    created a dictionary for the computer

--- pythonprobset6/createdict.py
+++ pythonprobset6/createdict.py
@@ -2,7 +2,7 @@
 SCRABBLE_LETTER_VALUES = {
     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
-word=" "
+word=[]
 dicc={}
 word_list=["aahh","haha","bombay","delhi","bah","cab","coder","good"]
 dicword=[]
@@ -21,13 +21,17 @@ def pointdict(word_list):
     for i in range(len(word_list)):
         lenword=len(word_list[i])
         word=word_list[i]
+        score=getwordscore(word,SCRABBLE_LETTER_VALUES)
         if lenword not in dicc.keys():
-            score=getwordscore(word,SCRABBLE_LETTER_VALUES)
+            dicword.append(word)
+            print dicword
             dicc[lenword]={score:[word]}
         else :
-            score=getwordscore(word,SCRABBLE_LETTER_VALUES)
-            if score in dicc[lenword].keys():
-                dicc[lenword]={score:dicword.append(word)}
+            if score not in dicc[lenword].keys():
+                dicc[lenword][score]=[word]
+            else:
+                dicc[lenword][score].append(word)
+
 
 pointdict(word_list)
 print(dicc)
