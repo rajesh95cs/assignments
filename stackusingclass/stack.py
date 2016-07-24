38382c411cae04de807e81e461b7a3bc71efb10b
rajesh95cs <rajeshrnair95@example.com>
2016-07-24 11:31:54 +0530

    stack using classes:

---
+++ stackusingclass/stack.py
@@ -0,0 +1,29 @@
+class stack(object):
+    def __init__(self):
+        self.stack=[]
+    def push(self,element):
+        self.stack.append(element)
+
+    def pop(self):
+        del self.stack[-1]
+
+    def __str__(self):
+        for i in self.stack:
+            str(self.stack[-i])
+
+
+    def __iter__(self):
+        return iter(self.stack)
+
+    def isempty(self):
+        if len(self.stack) == 0:
+            return True
+        else:
+            return False
+    def __eq__(self,other):
+        return type(other) == stack and cmp(self.stack,other.stack)==0
+
+    def add(self,other):
+        return self.stack = self.stack + other.stack.reverse()
+
+                   
