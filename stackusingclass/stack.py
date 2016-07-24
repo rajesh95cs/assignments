abd723bf6e21c20f95cd163e794a07c320b9681a
rajesh95cs <rajeshrnair95@example.com>
2016-07-24 13:46:45 +0530

    stack using class

--- stackusingclass/stack.py
+++ stackusingclass/stack.py
@@ -1,28 +1,33 @@
-class stack(object):
+class Stack(object):
     def __init__(self):
         self.container = []
     def push(self,element):
         self.container.append(element)
 
     def pop(self):
-        del self.contain[-1]
+         popedelemnt = self.container[-1]
+         del self.container[-1]
+         return popedelemnt
 
     def __str__(self):
+        if self.isempty():
+            return "the stack is empty"
+        else:
             return str(self.container)
 
 
     def __iter__(self):
         return iter(self.container)
 
-    def isempty():
+    def isempty(self):
         if len(self.container) == 0:
             return True
         else:
             return False
     def __eq__(self , other):
-        return type(other) == stack and cmp(self.container,other.stack)==0
+        return type(other) == Stack and cmp(self.container,other.container)==0
 
     def __add__(self , other):
-        netstack = stack()
-        netstack.container = self.container + other.container
-        return netstack
+        netStack = Stack()
+        netStack.container = self.container + other.container
+        return netStack
